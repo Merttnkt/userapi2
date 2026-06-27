5aecbf54264820218b04e19f130cd9ac0a78df97
merttnkt <mert.tankit@gmail.com>
2026-06-27 10:48:31 +0300

    Refactor User API with EF Core and SQLitet

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+namespace UserapiEfCore.Models;
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Lastname { get; set; } = "";
+    public int Age { get; set; }
+}
+
---
+++ Program.cs
@@ -0,0 +1,63 @@
+using Microsoft.EntityFrameworkCore;
+using UserapiEfCore.Data;
+using UserapiEfCore.Models;
+
+var builder = WebApplication.CreateBuilder(args);   
+var app = builder.Build();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=users.db"));
+
+app.MapGet("/users", (AppDbContext context) =>
+{
+    var users = context.Users.ToList();
+    return users;
+});
+
+app.MapPost("/users", (AppDbContext context , User user)  =>
+{
+    context.Users.Add(user);
+    context.SaveChanges();
+    return user;
+});
+
+app.MapDelete("/users/{id}", (AppDbContext context, int id) =>
+{
+    var user = context.Users.FirstOrDefault(x => x.Id == id);
+
+    if (user == null)
+    {
+        return Results.NotFound("User not found");
+    }
+
+    context.Users.Remove(user);
+    context.SaveChanges();
+
+    return Results.Ok("User successfully deleted");
+});
+
+app.MapGet("/users/{id}", (AppDbContext context, int id) =>
+{
+    var user = context.Users.FirstOrDefault(x => x.Id == id);
+    if (user == null){
+        return Results.NotFound("User not found");
+    }
+    return Results.Ok(user);
+});
+
+app.MapPut("/users/{id}", (AppDbContext context, int id , User updateUser) =>
+{
+    var user = context.Users.FirstOrDefault(x => x.Id == id);
+    if (user == null)
+    {
+        return Results.NotFound("User not found");
+    }
+    user.Name = updateUser.Name;
+    user.Lastname = updateUser.Lastname;
+    user.Age = updateUser.Age;
+    context.SaveChanges();
+    return Results.Ok("User successfully updated");
+
+});
+
+app.Run(); 
